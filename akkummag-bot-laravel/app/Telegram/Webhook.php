561e276a0c4248f70b6c868a74d5ea888d19996e
Nikita Mulenko <wwaffle9000@gmail.com>
2024-06-03 18:14:26 +0300

    1) Created a project 2) Added Telegraph package 3) installed Webhook 4) Created first command (not active for now)

---
+++ akkummag-bot-laravel/app/Telegram/Webhook.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Telegram;
+
+use DefStudio\Telegraph\Handlers\WebhookHandler;
+
+class Webhook extends WebhookHandler
+{
+    public function start():void{
+        //$this->chat->message("Вас приветствует бот магазина АККУМ-МАГ.\nБудем выбирать аккумулятор для вашего авто?🚗\n\n❗Бот не отвечает на ваши сообщения❌. Пользуйтесь меню ниже👇")->send();
+    }
+
+
+}
